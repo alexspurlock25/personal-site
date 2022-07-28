d2d9c33dedb75f1ddbaf541ae684b4727af08232
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-28 10:18:36 -0400

    Added a projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const ProjectsPage = () => {
+    return (
+        <main>
+            <title>Projects - Alexander Spurlock</title>
+            <h1>Projects</h1>
+            <p>A list of my projects</p>
+        </main>
+    )
+}
+
+export default ProjectsPage
