d96112a0404d0ce14811e1127bd8e73934854a68
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-28 10:21:52 -0400

    new dir and component

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const ProjectsPage = () => {
     return (
         <main>
             <title>Projects - Alexander Spurlock</title>
             <h1>Projects</h1>
+            <Link to='/'>Back Home</Link>
             <p>A list of my projects</p>
         </main>
     )
