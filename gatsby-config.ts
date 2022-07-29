10272991117e9e6f5da405d0b1ec59e3f91e64e0
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-29 10:46:23 -0400

    Added projects folder for gatsby to read

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,25 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `alexander-spurlock`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Alexander Spurlock`,
+    siteUrl: `https://alexanderspurlock.netlify.app/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `projects`,
+        path: `${__dirname}/projects`
+      }
+    },
+    "gatsby-plugin-mdx"
+  ],
 }
 
 export default config
