7edbc9902f004285449e55f175d6c8242b0c34e6
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-30 13:23:45 -0400

    using types

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 // data types
-import { ProjectData } from '../misc/Types'
+import { Project } from '../misc/Types'
 
-const ProjectsPage = (props: { data: ProjectData }) => {
+const ProjectsPage = (props: { data: Project }) => {
     return (
         <Layout pageTitle='Projects'>
             <p>Projects</p>
+            {
+                props.data.allMdx.nodes.map(node => (
+                    <p>{ node.id }</p>
+                ))
+            }
         </Layout>
     )
 }
+
+export const query: Project = graphql`
+    query {
+        allMdx {
+            nodes {
+                frontmatter {
+                    title
+                }
+                id
+            }
+        }
+    }
+`
 
 export default ProjectsPage
