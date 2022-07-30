03e6589cfd977214296edfc91d9587da842b6b42
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-30 18:55:33 -0400

    Basic styles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div className='grid justify-center content-center w-full h-60 bg-gradient-to-r from-stone-800 to-stone-900'>
+            <header className='text-slate-200 text-lg'>Alexander Spurlock</header>
+        </div>
+    )
 }
 
 export default Header
