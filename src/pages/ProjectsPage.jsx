aa48d4d0aa9495936c7c689ca9479f38ad021871
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-07-30 22:27:16 -0400

    Added base project style and content

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,7 +1,20 @@
 import * as React from 'react'
+import Card from '../components/Card'
+
+const projectsData = {
+    temp: 'This website is new as of July 30th. I will have a nice view of my projects here. For now, visit my github.',
+    ghLink: 'https://github.com/alexspurlock25'
+}
 
 const ProjectsPage = () => {
-    return <div>Projects...</div>
+    return (
+        <Card>
+            { projectsData.temp }
+            <div>
+                <a className='bg-green-200' href={ projectsData.ghLink } target="_blank">GitHub</a>
+            </div>
+        </Card>        
+    )
 }
 
 export default ProjectsPage
