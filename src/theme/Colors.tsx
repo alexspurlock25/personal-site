860028cef6ffd06a2f7b1b7631b6225c8d57f66b
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-01 15:53:42 -0400

    Added a colors with colors for the whole project

---
+++ src/theme/Colors.tsx
@@ -0,0 +1,7 @@
+// constant colors for the project
+// IDEAS: https://www.jordanprindledesigns.com/blog/colors-palettes-mood
+const DEEP_ROYAL = '#281B24'
+const THUNDER = '#3E3842'
+const STORMY_SKY = '#74858F'
+const FOAM = '#A4BDBE'
+const BABY_BIRD = '#BDD6DA'
