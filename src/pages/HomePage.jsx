57641d24de696a5c60649a45ae9e3d804d51a514
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-03 11:38:47 -0400

    Centered main content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import ProfilePicture from '../components/ProfilePicture'
 import TextCard from '../components/TextCard'
 
 const styles = {
-    container: 'container my-10',
+    container: 'container my-10 mx-auto',
 }
 const data = {
     aboutMe: 'Full time Engineer, and student at University of Cincinnati working on my Bachelor of Science in Information Technology (BSIT). I love making software, websites, and using new technologies to bring my ideas to life.'
