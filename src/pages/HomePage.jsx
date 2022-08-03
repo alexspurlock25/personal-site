ad87a70e6936816ec215181a19f772adc8531bcf
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-03 11:33:46 -0400

    Organized styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,18 @@
-import * as React from 'react'
 import ProfilePicture from '../components/ProfilePicture'
 import TextCard from '../components/TextCard'
 
-const containerStyles = 'container mx-auto grid justify-center content-center mt-5'
-const aboutMeData = {
+const styles = {
+    container: 'container my-10',
+}
+const data = {
     aboutMe: 'Full time Engineer, and student at University of Cincinnati working on my Bachelor of Science in Information Technology (BSIT). I love making software, websites, and using new technologies to bring my ideas to life.'
 }
 
 const HomePage = () => {
     return (
-        <div className={ containerStyles }>
+        <div className={ styles.container }>
             <ProfilePicture />
-            <TextCard text={ aboutMeData.aboutMe } />
+            <TextCard text={ data.aboutMe } />
         </div>
     )
 }
