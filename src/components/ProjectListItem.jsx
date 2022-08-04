5bb83aabf17780e1cd7127308a7edc6d7534a053
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-03 21:52:46 -0400

    Minor update to the styles

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const styles = {
-	container: 'border my-5 p-2 mx-auto max-w-sm rounded',
+	container: 'border my-5 p-2 mx-auto max-w-md rounded',
 	title: 'font-bold text-lg pb-5',
 	desc: ''
 }
