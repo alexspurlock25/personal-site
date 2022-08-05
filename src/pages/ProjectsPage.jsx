3693c9c199d88bd98ca7216a3cb76ffc1803801c
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-05 14:59:52 -0400

    Cleaned up ProjectPage component

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,18 +1,5 @@
-import * as React from 'react'
-import TextCard from '../components/TextCard'
 import ProjectList from '../components/ProjectList'
 
-const data = {
-    temp: 'This website is new as of July 30th. I will have a nice view of my projects here. For now, visit my github.',
-    ghLink: 'https://github.com/alexspurlock25'
-}
-
-const ProjectsPage = () => {
-    	return (
-            <div className=''>
-                <ProjectList />	
-            </div>
-        )	
-}
+const ProjectsPage = () => <ProjectList />	
 
 export default ProjectsPage
