ccb3b1be00c33186d7dbae2f892922ac2463343d
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-07 15:42:29 -0400

    Made changes to component
    Added a button link to source code with supplied link

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 const styles = {
 	container: 'border my-5 p-2 mx-auto max-w-md rounded',
+	containerHeader: 'flex justify-between',
 	title: 'font-bold text-lg pb-5',
-	desc: ''
+	desc: '',
+	link: 'bg-stormy-sky text-slate-200 text-center px-3 py-1 h-min'
 }
 
-const ProjectListItem = ({ title, desc }) => {
+const ProjectListItem = ({ title, desc, link }) => {
 	return (
 		<div className={ styles.container }>
-			<div className={ styles.title }>{ title }</div>
+			<div className={ styles.containerHeader }>
+				<div className={ styles.title }>{ title }</div>
+				<a className={ styles.link } href={link} target='_blank'>Code</a>
+			</div>
 			<div className={ styles.desc }>{ desc }</div>
 		</div>
 	)
