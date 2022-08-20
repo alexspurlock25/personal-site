4550cda5857606667371b4cde4f3346aeb2c7927
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-20 07:44:12 -0400

    Update ProjectListItem.jsx
    - Conditional divs

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -1,5 +1,3 @@
- import React from 'react'
-
 const styles = {
 	container: 'w-full border my-5 md:my-0 p-2 max-w-sm rounded bg-black text-white shadow hover:shadow-lg hover:scale-95 duration-200',
 	containerHeader: 'flex justify-between',
@@ -9,15 +7,15 @@ const styles = {
 	link: 'flex items-stretch'
 }
 
-const ProjectListItem = ({ title, desc, link, lang }) => {
+const ProjectListItem = ({ title, desc='', link, lang='' }) => {
 	return (
 		<a href={ link } target='_blank' className={ styles.link }>
 			<div className={ styles.container }>
 				<div className={ styles.containerHeader }>
 					<div className={ styles.title }>{ title }</div>
-					<div className={ styles.lang}>{ lang }</div>
+					{ lang ? <div className={ styles.lang}>{ lang }</div> : null }
 				</div>
-				<div className={ styles.desc }>{ desc }</div>				
+				{ desc ? <div className={ styles.desc }>{ desc }</div> : null }				
 			</div>
 		</a>
 	)
