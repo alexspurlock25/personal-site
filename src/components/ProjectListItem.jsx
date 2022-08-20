43186e24b98ae00f14ed526fa90f9c5f0ff02945
Alexander Spurlock <alexanderspurlock@gmail.com>
2022-08-20 09:05:39 -0400

    Update ProjectListItem.jsx
    - Fixed styles with mobile view having too much gap between cards
    - Added conditional divs

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -1,22 +1,22 @@
 const styles = {
-	container: 'w-full border my-5 md:my-0 p-2 max-w-sm rounded bg-black text-white shadow hover:shadow-lg hover:scale-95 duration-200',
 	containerHeader: 'flex justify-between',
 	title: 'font-bold text-lg',
 	desc: 'my-2',
-	lang: 'text-xs bg-baby-bird text-white h-min max-w-fit px-3 py-1 rounded',
-	link: 'flex items-stretch'
+	link: ' block p-6 max-w-sm rounded border bg-black text-white shadow hover:shadow-lg hover:scale-95 duration-200',
+	pill: 'text-xs bg-baby-bird text-white h-min max-w-fit px-3 py-1 rounded'
 }
 
-const ProjectListItem = ({ title, desc='', link, lang='' }) => {
+const ProjectListItem = ({ title, desc='', link, lang = '', hasDevLog = false }) => {
 	return (
 		<a href={ link } target='_blank' className={ styles.link }>
-			<div className={ styles.container }>
+			<div>
 				<div className={ styles.containerHeader }>
 					<div className={ styles.title }>{ title }</div>
-					{ lang ? <div className={ styles.lang}>{ lang }</div> : null }
+					{ lang ? <div className={ styles.pill }>{ lang }</div> : null }
 				</div>
 				{ desc ? <div className={ styles.desc }>{ desc }</div> : null }				
 			</div>
+			{ hasDevLog ? <div className={ styles.pill }>Dev Log</div> : null }
 		</a>
 	)
 }
